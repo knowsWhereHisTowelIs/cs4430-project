488e42657cc594ef4c6d02df45e7abf0436e87d0
root <root@wireless147-061.wireless.wmich.edu>
2017-11-29 21:40:17 -0500

    hello

--- public/templates/enrolled/delete.php
+++ public/templates/enrolled/delete.php
@@ -1,10 +1,10 @@
 <?php require __DIR__ . '/../header.php'; ?>
 
-<?php formatted_var_dump (get_defined_vars());?>
-
 <h1>Enrolled List</h1>
 <form method='post'>
     <p>Are you sure you want to delete?</p>
+    <input type="hidden" name="sid" value="<?php echo $sid; ?>" />
+    <input type="hidden" name="cid" value="<?php echo $cid; ?>" />
     <input name='submitted' type='submit' />
 </form>
 
